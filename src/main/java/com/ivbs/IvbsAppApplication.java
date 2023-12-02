8c5be3cefe3f598935720b028ea02acdbf854982
akashmore06 <akashmore041@gmail.com>
2023-12-02 15:31:14 +0530

    added

---
+++ src/main/java/com/ivbs/IvbsAppApplication.java
@@ -0,0 +1,13 @@
+package com.ivbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvbsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvbsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivbs/IvbsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvbsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
